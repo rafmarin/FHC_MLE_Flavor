cf14ad08ec9e427654e4223630cd134556b9d8a5
rafmarin <rafmarin@deloitte.com>
2021-12-16 15:41:59 -0500

    12/16 update

---
+++ src/flavor_analysis.py
@@ -0,0 +1,14 @@
+# This is where our working code should go
+
+import pandas as pd
+import numpy as np
+import re, random, nltk, os
+from gensim.models import Word2Vec
+
+from nltk import word_tokenize
+from nltk.corpus import stopwords
+nltk.download('punkt')
+
+nltk.download("stopwords")
+
+
